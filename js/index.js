a93e590946ef8f6116d55661f6859e304928fb8c
Lee-pin-xin <somiltory@gmail.com>
2017-08-18 16:14:42 +0800

    2.0

--- js/index.js
+++ js/index.js
@@ -1,3 +1,56 @@
+
 $(".menu").on("click", function() {
-  $(".menu").parent().toggleClass('active');
+  $(".navbar").toggleClass('active');
+});
+
+function myfunction(){
+  var win_width = $(window).width();
+  if (win_width>=970) {
+   $(".navbar").addClass('nav_list');
+   $(".navbar").removeClass('nav_menu'); 
+   console.log('PC');
+  }
+  else{
+    $(".navbar").addClass('nav_menu');
+    $(".navbar").removeClass('nav_list');
+    console.log('mobile');
+  }
+};
+
+$(window).scroll(function(){
+  var scroll_height=$(window).scrollTop();
+  var win_width =$(window).width();
+
+  if(scroll_height>=550&&win_width>=995){
+    $(".navbar").addClass('nav_menu');
+    $(".navbar").removeClass('nav_list');
+
+  }
+  else{
+    $(".navbar").addClass('nav_list');
+    $(".navbar").removeClass('nav_menu'); 
+  }
+  console.log(win_width);
+  
+
+});
+
+$(window).on('resize', function(){
+  
+
+   var win = $(this); //this = window
+   var win_width = win.width();
+   
+  //  console.log(scroll_height);
+
+   if (win_width>=995) {
+    $(".navbar").addClass('nav_list');
+    $(".navbar").removeClass('nav_menu'); 
+    console.log('PC');
+   }
+   else{
+     $(".navbar").addClass('nav_menu');
+     $(".navbar").removeClass('nav_list');
+     console.log('mobile');
+   }
 });
