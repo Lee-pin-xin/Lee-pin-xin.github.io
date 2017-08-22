ed68434c4d447a9c11e5e04dd1077b43b64e230f
pin-xin lee <pin-xin lee>
2017-08-22 08:04:53 +0800

    3.0
    Signed-off-by: pin-xin lee <pin-xin lee>

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
-
+var s = skrollr.init();
 $(".menu").on("click", function() {
   $(".navbar").toggleClass('active');
+});
+$(".buttom_li#1").on("click", function() {
+  d=1;
+  backdround_run(d);
+});
+$(".buttom_li#2").on("click", function() {
+  d=2;
+  backdround_run(d);
+});$(".buttom_li#3").on("click", function() {
+  d=3;
+  backdround_run(d);
+});$(".buttom_li#4").on("click", function() {
+  d=4;
+  backdround_run(d);
 });
 
 function myfunction(){
@@ -28,7 +42,8 @@ $(window).scroll(function(){
   }
   else if(scroll_height<=550&&win_width>=995){
     $(".navbar").addClass('nav_list');
-    $(".navbar").removeClass('nav_menu'); 
+    $(".navbar").removeClass('nav_menu');
+    $(".navbar").removeClass('active'); 
   }
   console.log(win_width);
   
@@ -54,3 +69,49 @@ $(window).on('resize', function(){
      console.log('mobile');
    }
 });
+var myVar = setInterval(myTimer ,5000);
+var d=0;
+function myTimer() {
+  
+    d++;
+    backdround_run(d);
+    if(d==5){
+      d=1;
+    }
+    
+    console.log(d);
+};
+var x=0;
+function backdround_run(x){
+    if(x==1){
+      $(".joumbotron").css("background","url('http://i.imgur.com/llzZUGC.jpg')");
+      $(".buttom_li#1").css("background-color","rgba(255,255,255,1)");
+      $(".buttom_li#2").css("background-color","rgba(255,255,255,0)");
+      $(".buttom_li#3").css("background-color","rgba(255,255,255,0)");
+      $(".buttom_li#4").css("background-color","rgba(255,255,255,0)");
+     
+    }
+    if(x==2){
+      $(".joumbotron").css("background","url('http://i.imgur.com/q44SKvg.jpg')");
+      $(".buttom_li#2").css("background-color","rgba(255,255,255,1)");
+      $(".buttom_li#1").css("background-color","rgba(255,255,255,0)");
+      $(".buttom_li#3").css("background-color","rgba(255,255,255,0)");
+      $(".buttom_li#4").css("background-color","rgba(255,255,255,0)");
+    }
+
+    if(x==3){
+      $(".joumbotron").css("background","url('http://i.imgur.com/S4tX7u6.jpg')");
+      $(".buttom_li#3").css("background-color","rgba(255,255,255,1)");
+      $(".buttom_li#2").css("background-color","rgba(255,255,255,0)");
+      $(".buttom_li#1").css("background-color","rgba(255,255,255,0)");
+      $(".buttom_li#4").css("background-color","rgba(255,255,255,0)");
+    }
+    if(x==4){
+      $(".joumbotron").css("background","url('http://i.imgur.com/impWz9e.jpg')");
+      $(".buttom_li#4").css("background-color","rgba(255,255,255,1)");
+      $(".buttom_li#2").css("background-color","rgba(255,255,255,0)");
+      $(".buttom_li#3").css("background-color","rgba(255,255,255,0)");
+      $(".buttom_li#1").css("background-color","rgba(255,255,255,0)");
+    }
+    $(".joumbotron").css("background-size","cover");
+}
